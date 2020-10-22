d98f8113decc1a884ffe01e0bdcf4adff72b5240
Pedro Queirós <pedromsqueiros@gmail.com>
2020-10-22 11:29:43 +0100

    Implemented function that process play

--- src/board.pl
+++ src/board.pl
@@ -9,7 +9,6 @@ initialBoard([
 
 
 print_board(X):-
-	initialBoard(X),
 	nl,
 	write('   | A | B | C | D | E | F |\n'),
 	write('---|---|---|---|---|---|---|\n'),
@@ -43,3 +42,32 @@ print_element(X):-
 
 print_symbol(1,S):-S=' X'.
 print_symbol(0,S):-S='  '.
+
+
+replace(_,_,[],[]).
+replace(X,Y,[X|L1],[Y|L2]):-
+	replace(X,Y,L1,L2).
+replace(X,Y,[H|L1],[H|L2]):
+	-X\=H,
+	replace(X,Y,L1,L2).
+
+
+playPiece(InitialMatrix,R,C,1,FinalMatrix):-
+	initialBoard(InitialMatrix),
+	print_board(InitialMatrix),
+	playRow(R,InitialMatrix,C,1,FinalMatrix),
+	print_board(FinalMatrix).
+	
+playRow(1,[Row|RestRow],C,1,[NewRow|RestRow]):-
+	playColumn(C,Row,1,NewRow).
+	
+playRow(NRow,[Row|RestRow],C,1,[Row|NewRow]):-
+	NRow > 1,
+	N is NRow-1,
+	playRow(N,RestRow,C,1,NewRow).
+	
+playColumn(1,[_|RestColumn],1,[1|RestColumn]).
+playColumn(NColumn,[P|RestColumn],1,[P|NewColumn]):-
+	NColumn > 1,
+	N is NColumn-1,
+	playColumn(N,RestColumn,1,NewColumn).
